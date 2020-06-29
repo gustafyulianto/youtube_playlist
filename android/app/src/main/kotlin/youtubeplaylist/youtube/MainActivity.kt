5961f1feaa85d189ed760552d7615ac936245d9c
Gustaf <gustaf.yulianto@pgascom.co.id>
2020-06-29 23:01:39 +0700

    update_1
    update_1

---
+++ android/app/src/main/kotlin/youtubeplaylist/youtube/MainActivity.kt
@@ -0,0 +1,13 @@
+package youtubeplaylist.youtube
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
